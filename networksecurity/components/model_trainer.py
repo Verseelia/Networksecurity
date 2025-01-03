18b0bc0da6c2f9011f40484c4ee272f798290eac
Verseelia <verseeliafrancis1503@gmail.com>
2025-01-03 14:49:57 +0530

    Pipeline is committed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -25,6 +25,10 @@ from sklearn.ensemble import (
 )
 import mlflow
 
+import dagshub
+dagshub.init(repo_owner='verseeliafrancis1503', repo_name='Networksecurity', mlflow=True)
+
+
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
         try:
